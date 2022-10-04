f83d5c54df6868a7e07bfb10173f28ae065cad38
hasiniV19 <83814896+hasiniV19@users.noreply.github.com>
2022-10-04 11:00:34 +0530

    scheduling small part

---
+++ schedule.py
@@ -0,0 +1,26 @@
+from constraint import *
+
+problem = Problem()
+problem.addVariable('Doctor', ['Doc1', 'Doc2', 'Doc3'])
+problem.addVariable('Shift', ['Day1-shift1','Day1-shift2', 'Day1-shift3', 'Day2-shift1', 'Day2-shift2', 'Day2-shift3'])
+
+def constraints_func(Doctor, Shift):
+    CONSTRAINTS = [
+        ("Doc1", "Day1-shift2"),
+        ("Doc2", "Day2-shift3"),
+        ("Doc3", "Day1-shift1"),
+        ("Doc3", "Day1-shift3")
+    ]
+
+
+    for doctor, shift in CONSTRAINTS:
+        if doctor == Doctor and shift == Shift:
+            return False
+
+    return True
+
+
+problem.addConstraint(constraints_func, ['Doctor', 'Shift'])
+
+for solution in problem.getSolutions():
+    print(solution)
